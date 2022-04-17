e002afef3816e30979c9cb83508888f2368b64c6
MangoBicarbonate <77314122+MangoBicarbonate@users.noreply.github.com>
2022-04-17 00:43:58 -0500

    Delete intrest.js

--- intrest.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-
-    const params = (new URL(document.location)).searchParams;
-    const math1 = params.get("math1");
-    const math2 = params.get("math2");
-
-    document.getElementById("math1").innerHTML = math1;
-    document.getElementById("math2").innerHTML = math2;
-})
