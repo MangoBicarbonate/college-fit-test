b586dd66be5f0ff01b75bef8a8f519c2eb7283ae
MangoBicarbonate <77314122+MangoBicarbonate@users.noreply.github.com>
2022-04-17 02:41:06 -0500

    Create actual.js

---
+++ actual.js
@@ -0,0 +1,10 @@
+function greetings(){
+    var course1 = document.getElementById("c1").value;
+    var course2 = document.getElementById("c2").value;
+    var course3 = document.getElementById("c3").value;
+    var course4 = document.getElementById("c4").value;
+    var course5 = document.getElementById("c5").value;
+    var course6 = document.getElementById("c6").value;
+
+    document.write("Hello!!, " + course1 + course2 + course3);
+    }
